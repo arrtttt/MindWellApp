618b70531eef0753260ecce6adea90ec4bc40baf
WandeTheWeeb <josefranciscosantesprimo@gmail.com>
2024-02-11 19:36:03 -0600

    Lineas de desarrollo independientes inician aqui

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,56 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:mindwell/utils/theme.dart';
 
 class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({Key? key}) : super(key: key);
+  const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile'),
-      ),
-      body: ListView(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(16.0),
-            child: const Column(children: [
-              CircleAvatar(
-                  radius: 100.0,
-                  backgroundImage: AssetImage('assets/images/profile.png')),
-              SizedBox(height: 16.0),
-              Text(
-                'John Doe',
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
+        appBar: AppBar(
+          title: const Text('Profile'),
+        ),
+        body: SingleChildScrollView(
+            child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: lightColorScheme.primaryContainer,
                 ),
-              ),
-              SizedBox(height: 8.0),
-              Text('Edoardo'),
-              SizedBox(height: 8.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Icon(Icons.location_on),
-                  Text('Milan, Italy'),
-                ],
-              ),
-              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-                Icon(Icons.email),
-                Text('JohnDoe@gmail.com'),
-              ]),
-              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-                Icon(Icons.phone),
-                Text('+39 123 456 7890'),
-              ]),
-              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-                Icon(Icons.web),
-                Text('https://www.johndoe.com'),
-              ]),
-            ]),
-          ),
-        ],
-      ),
-    );
+                padding: const EdgeInsets.all(16),
+                child: const Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        radius: 50,
+                        backgroundImage:
+                            AssetImage('assets/images/profile.png'),
+                      ),
+                      SizedBox(height: 16),
+                      Text(
+                        'John Doe',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      Text(
+                        '¿Qué es MindWell?',
+                      )
+                    ]))));
   }
 }
