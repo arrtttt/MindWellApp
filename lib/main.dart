2e7053ef08a4ef7d1e9ae0268c8942d02f7051bb
WandeTheWeeb <josefranciscosantesprimo@gmail.com>
2024-02-14 19:45:05 -0600

    add BottomNavBar

--- lib/main.dart
+++ lib/main.dart
@@ -14,61 +14,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: Routes.inicio,
+      initialRoute: Routes.home,
       routes: appRoutes,
       theme: mindWellTheme,
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text('MindWell'),
-          ),
-          body: Builder(builder: (context) {
-            return Column(
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, Routes.settings);
-                  },
-                  child: const Text('Settings'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, Routes.chat);
-                  },
-                  child: const Text('Chat'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, Routes.psicoProfile);
-                  },
-                  child: const Text('Psicologo Profile'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, Routes.login);
-                  },
-                  child: const Text('Login'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, Routes.register);
-                  },
-                  child: const Text('Register'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/list');
-                  },
-                  child: const Text('Psicologos Disponibles'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, Routes.information);
-                  },
-                  child: const Text('Information'),
-                ),
-              ],
-            );
-          })),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:mindwell/screens/chat_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+import 'init_screen.dart';
+import 'profile_screen.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentPageIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        body: [
+          const InitScreen(),
+          const ChatScreen(),
+          const ProfileScreen(),
+        ][currentPageIndex],
+        bottomNavigationBar: NavigationBar(
+          destinations: const [
+            NavigationDestination(
+              label: 'Home',
+              icon: Icon(Icons.home_outlined),
+              selectedIcon: Icon(Icons.home),
+            ),
+            NavigationDestination(
+              label: 'Chat',
+              icon: Icon(Icons.chat_outlined),
+              selectedIcon: Icon(Icons.chat),
+            ),
+            NavigationDestination(
+              label: 'Profile',
+              icon: Icon(Icons.person_outline),
+              selectedIcon: Icon(Icons.person),
+            ),
+          ],
+          selectedIndex: currentPageIndex,
+          onDestinationSelected: (int index) {
+            setState(() {
+              currentPageIndex = index;
+            });
+          },
+        ));
   }
 }
