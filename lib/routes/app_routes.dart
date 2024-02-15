9ba487f7d9a9686982172e32ff36dcbb15ab5bc5
Arrttyy <ironman60@live.com.mx>
2024-02-14 19:53:50 -0600

    Arreglo de advertencias

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -12,7 +12,6 @@ import 'package:mindwell/screens/setting/language_settings_screen.dart';
 import 'package:mindwell/screens/setting/notification_settings_screen.dart';
 import 'package:mindwell/screens/setting/privacy_settings_screen.dart';
 import 'package:mindwell/screens/settings_screen.dart';
-import 'package:mindwell/screens/Listado.dart';
 
 Map<String, WidgetBuilder> appRoutes = {
   '/home': (BuildContext context) => const MyApp(),
@@ -29,6 +28,5 @@ Map<String, WidgetBuilder> appRoutes = {
   '/language_settings': (BuildContext context) =>
       const LanguageSettingsScreen(),
   '/privacy_settings': (BuildContext context) => const PrivacySettingsScreen(),
-  '/help': (BuildContext context) => const HelpSettingsScreen(),
-  '/list': (BuildContext context) => const ListadoWidget()
+  '/help': (BuildContext context) => const HelpSettingsScreen()
 };
